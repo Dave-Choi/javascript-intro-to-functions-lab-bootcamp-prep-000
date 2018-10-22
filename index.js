38aca82f54a52e745c8326791cef61f61c33bd7f
David Choi <daaave@gmail.com>
2018-10-22 23:05:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function shout(message){
   return message.toUpperCase();
 }
+
+function whisper(message){
+  return message.toLowerCase();
+}
+
+function logShout(message){
+  console.log(shout(message));
+}
+
